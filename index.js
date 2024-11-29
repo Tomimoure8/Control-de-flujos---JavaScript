3c0e6bf692a7e20f7a79fc4e419f9fe7eda1998b
tomimoure8 <tomimoure8@gmail.com>
2024-11-29 13:48:09 -0300

    Practica de control de flujos - Actividad: Yo no fui

---
+++ index.js
@@ -0,0 +1,111 @@
+// ACTIVIDAD 1: Yo no fui - 29/11/2024
+
+let solicitarNombre = prompt ("solicito tu nombre");
+
+let nombreAlmacenado = "Bart";
+let nombreAlmacenadoDos = "Homero";
+let nombreAlmacenadoTres = "Marge";
+
+if (nombreAlmacenado == solicitarNombre) {
+    alert ("Fui yo");
+}
+
+else if (solicitarNombre == nombreAlmacenadoDos) {
+    alert ("Fui yo, Homero");
+}
+
+else if (solicitarNombre == nombreAlmacenadoTres) {
+    alert ("Fui yo, marge");
+}
+
+else {
+    alert ("Yo no fui");
+};
+
+
+
+
+// let diaDeLaSemana = parseInt (prompt("Inserta un número del 1 al 7"));
+// switch (diaDeLaSemana) {
+//     case 1:
+//         console.log ("Lunes");
+//     case 2:
+//         console.log ("martes");
+//     case 3:
+//         console.log ("miercoles");
+//         break;
+//     case 4:
+//         console.log ("jueves");
+//         break;
+//     case 5:
+//         console.log ("viernes");
+//         break;
+//     case 6:
+//         console.log ("sabado");
+//         break;
+//     case 7:
+//         console.log ("domingo");
+//         break;
+//     default:
+//         console.log ("no clickeaste un número del 1 al 7");
+// }
+
+
+
+//
+// let Insercion = prompt("Decime algun mes del año");
+// switch (Insercion) {
+//     case "enero":
+//         console.log ("enebro");
+//         break;
+//     case "febrero":
+//         console.log ("fiebre");
+//         break;
+//     case "marzo":
+//         console.log ("mazo");
+//         break;
+//     case "abril":
+//         console.log ("abrir");
+//         break;
+//     case "mayo":
+//         console.log ("maya");
+//         break;
+//     case "junio":
+//         console.log ("junco");
+//         break;
+//     case "julio":
+//         console.log ("julio cesar");
+//         break;
+//     case "agosto":
+//         console.log ("augusto cesar");
+//         break;
+//     case "septiembre":
+//         console.log ("serpiente");
+//         break;
+//     case "octubre":
+//         console.log ("octopus");
+//         break;
+//     case "noviembre":
+//         console.log ("novia");
+//         break;
+//     case "diciembre":
+//         console.log ("diciendo");
+//         break;
+// }
+
+
+// CLASE 2 REPASO ****************************************************
+
+// let messi = 10;
+// if (messi < 10 || messi > 10) {
+//     console.log ("A messi la 10 no le queda"); 
+// }
+// else if (messi === 9 && messi != 10) {
+//     console.log ("messi juega de nueve, messi no es diez");
+// }
+// else if (messi === 10) {
+//     console.log ("Efectivamente, messi es el 10");
+// }
+// else {
+//     console.log ("x");
+// }
