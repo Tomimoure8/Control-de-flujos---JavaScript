1b9ea51173f7fe37e2a0c4cf219b400af8b8393e
tomimoure8 <tomimoure8@gmail.com>
2024-12-04 17:48:17 -0300

    segunda-actividad-clase-3

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 // CLASE 3: Ciclos e interacciones
 
+// Actividad 3: Registro de alumnos // Solicitar al usuario la carga de diez (10) alumnos de forma consecutiva. // Luego realizar un única salida por alerta, con el listado de alumnos registrados.
+
+// let cargarAlumnos = prompt ("cargar alumno al sistema");
+
+
+
+// Actividad 2: El cuadrado ordinario // Solicitar al usuario un (1) número. // Emplear este valor para determinar la cantidad de repeticiones, y efectuar una salida por alerta con el mensaje “lado” en cada repetición. // Pero si se alcanza un número de iteraciones mayor que cuatro (4), cancelar el ciclo.
+
+let solicNumero = prompt ("Decime un número");
+
+for (let limite = 0; limite < solicNumero; limite++) {
+    if (limite > 3) {
+        break;
+    }
+    alert ("lado");
+}
+
+
+
 // Actividad 1: El pizarrón // Solicitar al usuario un (1) número y un (1) texto. // Efectuar una salida por alerta con el mensaje ingresado por cada repetición, hasta alcanzar el valor señalado por el usuario.
 
-let cantidad = parseInt (prompt ("Decime un número"));
-let texto = prompt ("Decime un texto");
+// let cantidad = parseInt (prompt ("Decime un número"));
+// let texto = prompt ("Decime un texto");
 
-for (let z = 0; z <= cantidad; z++) {
-    alert (texto);
-}
+// for (let z = 0; z <= cantidad; z++) {
+//     alert (texto);
+// }
 
 
 
@@ -16,140 +35,140 @@ for (let z = 0; z <= cantidad; z++) {
 
 // Actividad 5: Vacío // Solicitar al usuario cuatro (4) productos de almacén. // Si todos los elementos fueron cargados, realizar una única salida compuesta por el listado de productos. // Caso contrario, la salida será “Error: Es necesario cargar todos los productos”.
 
-let solicitarProducto1 = prompt ("¿Que producto te vas a llevar?");
-let solicitarProducto2 = prompt ("¿Que otro producto te vas a llevar?");
-let solicitarProducto3 = prompt ("¿Que otro producto te vas a llevar?");
-let solicitarProducto4 = prompt ("¿Que otro producto te vas a llevar?");
+// let solicitarProducto1 = prompt ("¿Que producto te vas a llevar?");
+// let solicitarProducto2 = prompt ("¿Que otro producto te vas a llevar?");
+// let solicitarProducto3 = prompt ("¿Que otro producto te vas a llevar?");
+// let solicitarProducto4 = prompt ("¿Que otro producto te vas a llevar?");
 
-if ((solicitarProducto1 != "") && (solicitarProducto2 != "") && (solicitarProducto2 != "") && (solicitarProducto4 != "")) {
-    console.log ("usted está llevando " +solicitarProducto1 +"," +solicitarProducto2+","+solicitarProducto3+" y una "+solicitarProducto4);
-}
-else {
-    console.log ("Es necesario cargar todos los productos")
-}
+// if ((solicitarProducto1 != "") && (solicitarProducto2 != "") && (solicitarProducto2 != "") && (solicitarProducto4 != "")) {
+//     console.log ("usted está llevando " +solicitarProducto1 +"," +solicitarProducto2+","+solicitarProducto3+" y una "+solicitarProducto4);
+// }
+// else {
+//     console.log ("Es necesario cargar todos los productos")
+// }
 
 
 
 // Actividad 4 con switch, resolucion 2
 
-let solicitarPresupuesto = parseInt (prompt ("Ingresa un presupuesto"));
+// let solicitarPresupuesto = parseInt (prompt ("Ingresa un presupuesto"));
 
-switch (true) {
-    case (solicitarPresupuesto <= 1000 && solicitarPresupuesto >= 0):
-        alert ("Presupuesto bajo");
-        break;
-    case (solicitarPresupuesto >= 1001 && solicitarPresupuesto < 3000):
-        alert ("Presupuesto medio");
-        break;
-    case (solicitarPresupuesto >= 3000):
-        alert ("Presupuesto alto");
-        break;
-    default:
-        alert ("Error al reconocer el presupuesto")
-}
+// switch (true) {
+//     case (solicitarPresupuesto <= 1000 && solicitarPresupuesto >= 0):
+//         alert ("Presupuesto bajo");
+//         break;
+//     case (solicitarPresupuesto >= 1001 && solicitarPresupuesto < 3000):
+//         alert ("Presupuesto medio");
+//         break;
+//     case (solicitarPresupuesto >= 3000):
+//         alert ("Presupuesto alto");
+//         break;
+//     default:
+//         alert ("Error al reconocer el presupuesto")
+// }
 
 // Actividad 4: Presupuesto // Solicitar al usuario un (1) número. // Si el valor está entre dos números, efectuar una de las siguientes salidas, según corresponda: // - “Presupuesto bajo” si está entre 0 y 1000. // - “Presupuesto medio” si está entre 1001 y 3000. // - “Presupuesto alto” si es mayor que 3000.
 
-let solicitarNumeroNoe = prompt ("Solicitar numero");
+// let solicitarNumeroNoe = prompt ("Solicitar numero");
 
-if ((solicitarNumeroNoe <= 1000) && (solicitarNumeroNoe >= 0)) {
-    console.log ("Presupuesto bajo");
-}
-else if ((solicitarNumeroNoe >= 1001) && (solicitarNumeroNoe < 3000)) {
-    console.log ("Presupuesto medio");
-}
-else if (solicitarNumeroNoe >= 3000) {
-    console.log ("Presupuesto alto");
-}
-else {
-    console.log ("Error al reconocer el presupuesto")
-}
+// if ((solicitarNumeroNoe <= 1000) && (solicitarNumeroNoe >= 0)) {
+//     console.log ("Presupuesto bajo");
+// }
+// else if ((solicitarNumeroNoe >= 1001) && (solicitarNumeroNoe < 3000)) {
+//     console.log ("Presupuesto medio");
+// }
+// else if (solicitarNumeroNoe >= 3000) {
+//     console.log ("Presupuesto alto");
+// }
+// else {
+//     console.log ("Error al reconocer el presupuesto")
+// }
 
 
 
 // Actividad 3: Elegir personaje // Solicitar al usuario un (1) número. // Si el valor está entre 1 y 4, efectuar una de las siguientes salidas según corresponda: // - “Elegiste a Homero” si es 1. // - “Elegiste a Marge” si es 2. // - “Elegiste a Bart” si es 3. // - “Elegiste a Lisa” si es 4.
 
-let solicitarNumero = parseInt (prompt ("Solicitar número"));
+// let solicitarNumero = parseInt (prompt ("Solicitar número"));
 
-if (solicitarNumero === 1) {
-    alert ("Elegiste a Homero");
-}
-else if (solicitarNumero == 2) {
-    alert ("Elegiste a Marge");
-}
-else if (solicitarNumero == 3) {
-    alert ("Elegiste a Bart");
-}
-else if (solicitarNumero == 4) {
-    alert ("Elegiste a Lisa");
-}
-else {
-    alert ("No elegiste ninguno");
-}
+// if (solicitarNumero === 1) {
+//     alert ("Elegiste a Homero");
+// }
+// else if (solicitarNumero == 2) {
+//     alert ("Elegiste a Marge");
+// }
+// else if (solicitarNumero == 3) {
+//     alert ("Elegiste a Bart");
+// }
+// else if (solicitarNumero == 4) {
+//     alert ("Elegiste a Lisa");
+// }
+// else {
+//     alert ("No elegiste ninguno");
+// }
 
 // Actividad 3 con switch - resolución 2:
 
-let solicitarNumeroSwitch = prompt ("Solicitar un Numero del 1 al 4");
+// let solicitarNumeroSwitch = prompt ("Solicitar un Numero del 1 al 4");
 
-switch (parseInt(solicitarNumeroSwitch)) {
-    case 1:
-        console.log ("Elegiste a Homero");
-        break;
-    case 2:
-        console.log ("Elegiste a Marge");
-        break;
-    case 3:
-        console.log ("Elegiste a Bart");
-        break;
-    case 4:
-        console.log ("Elegiste a Lisa");
-        break;
-    default:
-        console.log ("No elegiste ninguno de estos");
-}
+// switch (parseInt(solicitarNumeroSwitch)) {
+//     case 1:
+//         console.log ("Elegiste a Homero");
+//         break;
+//     case 2:
+//         console.log ("Elegiste a Marge");
+//         break;
+//     case 3:
+//         console.log ("Elegiste a Bart");
+//         break;
+//     case 4:
+//         console.log ("Elegiste a Lisa");
+//         break;
+//     default:
+//         console.log ("No elegiste ninguno de estos");
+// }
 
 
 
 // Actividad 2: Presionar Y // Solicitar al usuario una (1) tecla. // Si se presiona “y” (minúscula), o “Y” (mayúscula), realizar una salida por alerta con el mensaje “Correcto”. // Caso contrario, la salida será “Error”.
 
-let yMinuscula = "y";
-let yMayuscula = "Y";
+// let yMinuscula = "y";
+// let yMayuscula = "Y";
 
-let solicitarLetra = prompt ("Solicito una letra");
+// let solicitarLetra = prompt ("Solicito una letra");
 
-if ((solicitarLetra == yMinuscula) || (solicitarLetra == yMayuscula)) {
-    alert ("Correcto")
-}
+// if ((solicitarLetra == yMinuscula) || (solicitarLetra == yMayuscula)) {
+//     alert ("Correcto")
+// }
 
-else {
-    alert ("Error")
-};
+// else {
+//     alert ("Error")
+// };
 
 
 
 // Actividad 1: Yo no fui - 29/11/2024 // Solicitar al usuario un (1) nombre. // Si el mismo es igual a otro nombre almacenado en una variable, realizar una salida por alerta con el mensaje “Fui yo”. // Caso contrario, la salida será “Yo no fui”.
 
-let solicitarNombre = prompt ("solicito tu nombre");
+// let solicitarNombre = prompt ("solicito tu nombre");
 
-let nombreAlmacenado = "Bart";
-let nombreAlmacenadoDos = "Homero";
-let nombreAlmacenadoTres = "Marge";
+// let nombreAlmacenado = "Bart";
+// let nombreAlmacenadoDos = "Homero";
+// let nombreAlmacenadoTres = "Marge";
 
-if (nombreAlmacenado == solicitarNombre) {
-    alert ("Fui yo");
-}
+// if (nombreAlmacenado == solicitarNombre) {
+//     alert ("Fui yo");
+// }
 
-else if (solicitarNombre == nombreAlmacenadoDos) {
-    alert ("Fui yo, Homero");
-}
+// else if (solicitarNombre == nombreAlmacenadoDos) {
+//     alert ("Fui yo, Homero");
+// }
 
-else if (solicitarNombre == nombreAlmacenadoTres) {
-    alert ("Fui yo, marge");
-}
+// else if (solicitarNombre == nombreAlmacenadoTres) {
+//     alert ("Fui yo, marge");
+// }
 
-else {
-    alert ("Yo no fui");
-};
+// else {
+//     alert ("Yo no fui");
+// };
 
 
 
